23715342bbdbdb4067fa4450377a2a9b2d9756ee
coleki <coleki@gmail.com>
2016-04-07 09:23:38 -0700

    added cookie support
    added cookie support

--- app/src/main/java/com/example/kip/onlinegowebapp/MainActivity.java
+++ app/src/main/java/com/example/kip/onlinegowebapp/MainActivity.java
@@ -6,6 +6,8 @@ import android.view.Window;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.webkit.CookieManager;
+
 
 
 public class MainActivity extends Activity {
@@ -14,11 +16,13 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         WebView browser = (WebView)findViewById(R.id.browser);
         WebSettings webSettings = browser.getSettings();
         webSettings.setJavaScriptEnabled(true);
         browser.setWebViewClient(new WebViewClient());
+
+        CookieManager.getInstance().setAcceptCookie(true);
+
         browser.loadUrl("http://www.online-go.com");
 
     }
